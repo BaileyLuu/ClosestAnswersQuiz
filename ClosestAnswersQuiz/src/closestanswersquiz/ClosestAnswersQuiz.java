71c9a954fe59083882b38cac8783af599ed0f7f6
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2021-06-27 18:01:48 -0400

    Update ClosestAnswersQuiz.java

--- ClosestAnswersQuiz/src/closestanswersquiz/ClosestAnswersQuiz.java
+++ ClosestAnswersQuiz/src/closestanswersquiz/ClosestAnswersQuiz.java
@@ -21,7 +21,7 @@ public class ClosestAnswersQuiz {
         // TODO code application logic here
         
         System.out.println("\t\tWelcome to Closest Answers Quiz!\n");
-        System.out.println("This is a two-player game and there will be questions which players have to guess. The closest answer will earn a point!\n");
+        System.out.println("This is a two-player game and there will be questions which players have to guess. The person with the the closest answer will earn a point! Whoever has the most points wins! \n");
         System.out.print("Player 1\'s name: ");
         Scanner sc = new Scanner(System.in);
         String player1 = sc.nextLine();
